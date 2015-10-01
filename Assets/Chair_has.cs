09eab8a4cac8445f1ee9cc0c6e97ddf76a30463a
mixacom <wmmix@mail.ru>
2015-10-01 19:03:17 +0200

    Show and hide our objects

---
+++ Assets/Chair_has.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chair_has : MonoBehaviour {
+
+    public GameObject menu; // Assign in inspector
+    private bool isShowing;
+    public Renderer rend; 
+
+
+    // Use this for initialization
+    void Start () {
+        rend = this.GetComponent<Renderer>(); 
+    }
+
+    // Update is called once per frame 
+
+    void update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            rend.enabled = !rend.enabled; 
+        }
+
+    }
+
+    void Show()
+    {
+        GetComponent<Renderer>().enabled = true; 
+    } 
+
+    void Hide()
+    {
+        GetComponent<Renderer>().enabled = true; 
+    } 
+
+    void HideChildren()
+    {
+        Renderer[] lChildRenderers = gameObject.GetComponentsInChildren<Renderer>();
+        foreach (Renderer lRenderer in lChildRenderers)
+        {
+            lRenderer.enabled = false;
+        }
+    } 
+
+    void ShowChildren()
+    {
+        Renderer[] lChildRenderers = gameObject.GetComponentsInChildren<Renderer>();
+        foreach (Renderer lRenderer in lChildRenderers)
+        {
+            lRenderer.enabled = true;
+        }
+    }
+  
+}
---
+++ Assets/ToggleView.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleView : MonoBehaviour
+{
+
+    GameObject outside;
+    GameObject material;
+
+    bool isHidden;
+
+    bool showText = true;
+    Rect textArea = new Rect(0, 0, Screen.width, Screen.height); 
+
+    // Use this for initialization
+    void Start()
+    {
+        outside = GameObject.Find("Group46");
+        material = GameObject.Find("Mesh47");
+
+        isHidden = false; 
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+
+            if (!isHidden)
+            {
+                outside.SetActive(false);
+                isHidden = true; 
+            }
+            else
+            {
+                outside.SetActive(true);
+                isHidden = false; 
+            }
+
+        }
+        
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(800, 200, 100, 100), "Follow the ball, dear user.");
+    }
+
+
+
+}
